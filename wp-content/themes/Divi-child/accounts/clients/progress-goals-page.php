34ea984e2c799ace0ea4a0c743dc8c388efbb3ef
jabs <jabs@ilfusion.com>
2018-07-05 21:03:35 +0800

    Progress/Goal Update

--- wp-content/themes/Divi-child/accounts/clients/progress-goals-page.php
+++ wp-content/themes/Divi-child/accounts/clients/progress-goals-page.php
@@ -187,81 +187,131 @@
     </div>
 
 	<div class="current-status">
-		<div class="row">
-			<div class="col-lg-5 col-md-5">
+		<div class="row">			
+			<div class="col-lg-8 col-md-8">
 				<div class="current-goal">
-					<h3>Current Goal</h3>
+					<h3>Goal</h3>
 					<table style="width: 100%;">
 						<tr>
-							<th>Start #s</th>
 							<th></th>
+							<th>Start #s</th>
 							<th>Goal #s</th>
 						</tr>
 						<tr>
-							<td><span>167 lbs</span></td>
 							<td><label>Weight</label></td>
-							<td><span>180 lbs</span></td>
+							<td><input type="text" value="167 lbs" /></td>	
+							<td><input type="text" value="180 lbs" /></td>
 						</tr>
 						<tr>
-							<td><span>26%</span></td>
 							<td><label>Body Fat (%)</label></td>
-							<td><span>26%</span></td>
+							<td><input type="text" value="26%" /></td>
+							<td><input type="text" value="26%" /></td>
 						</tr>
 						<tr>
-							<td><span>34"</span></td>
 							<td><label>Waist</label></td>
-							<td><span>32"</span></td>
+							<td><input type="text" value="34 in" /></td>							
+							<td><input type="text" value="32 in" /></td>
 						</tr>
 						<tr>
-							<td><span>36"</span></td>
 							<td><label>chest</label></td>
-							<td><span>38"</span></td>
+							<td><input type="text" value="36 in" /></td>
+							<td><input type="text" value="38 in" /></td>
 						</tr>
 						<tr>
-							<td><span>34"</span></td>
 							<td><label>arms</label></td>
-							<td><span>32"</span></td>
+							<td><input type="text" value="34 in" /></td>
+							<td><input type="text" value="32 in" /></td>
 						</tr>
 						<tr>
-							<td><span>34"</span></td>
 							<td><label>forearms</label></td>
-							<td><span>32"</span></td>
+							<td><input type="text" value="34 in" /></td>
+							<td><input type="text" value="32 in" /></td>
 						</tr>
 						<tr>
-							<td><span>34"</span></td>
 							<td><label>shoulders</label></td>
-							<td><span>32"</span></td>
+							<td><input type="text" value="34 in" /></td>
+							<td><input type="text" value="32in" /></td>
 						</tr>
 						<tr>
-							<td><span>34"</span></td>
 							<td><label>hips</label></td>
-							<td><span>32"</span></td>
+							<td><input type="text" value="34 in" /></td>
+							<td><input type="text" value="32 in" /></td>
 						</tr>
 						<tr>
-							<td><span>24"</span></td>
 							<td><label>thighs</label></td>
-							<td><span>28"</span></td>
+							<td><input type="text" value="24 in" /></td>
+							<td><input type="text" value="28 in" /></td>
 						</tr>
 						<tr>
-							<td><span>16"</span></td>
 							<td><label>calves</label></td>
-							<td><span>18"</span></td>
+							<td><input type="text" value="16 in" /></td>
+							<td><input type="text" value="18 in" /></td>
 						</tr>
 						<tr>
-							<td><span>12"</span></td>
-							<td><label>nect</label></td>
-							<td><span>14"</span></td>
+							<td><label>neck</label></td>
+							<td><input type="text" value="12 in" /></td>
+							<td><input type="text" value="14 in" /></td>
 						</tr>
 						<tr>
-							<td><span>5'10"</span></td>
 							<td><label>height</label></td>
-							<td><span>5'10"</span></td>
+							<td><input type="text" value="5 ft 10 in" /></td>
+							<td><input type="text" value="5 ft 10 in" /></td>
 						</tr>
-					</table>
+					</table>	
 				</div>
 			</div>
-			<div class="col-lg-7 col-md-7">
-
+			<div class="col-lg-4 col-md-4">				
+				<div class="current-goal">
+					<h3>Today</h3>
+					<table style="width: 100%;">
+						<tr>
+							<th>Current #s</th>
+						</tr>
+						<tr>
+							<td><input type="text" value="34 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="26%" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="34 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="36 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="34 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="34 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="34 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="34 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="24 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="16 in%" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="12 in" /></td>
+						</tr>
+						<tr>							
+							<td><input type="text" value="5 ft 10 in" /></td>
+						</tr>
+					</table>					
+				</div>				
+			</div>
+			<div class="col-lg-12 col-md-12">
+				<div style="text-align:center;padding:10px 0;">
+					<button class="red-btn">Update</button>					
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12">
 				<div class="progress-photos">
 					<h3>progress photos</h3>
 
@@ -291,12 +341,17 @@
 							<span>Take New</span>
 						</li>
 					</ul>
-
-				</div>
+					<div style="text-align:center;padding:10px 0;">
+						<button class="red-btn">Update</button>					
+					</div>
+				</div>				
 
 				<div class="progress-notes">
 					<p class="label">In details, explain what are your trying to accomplish</p>
 					<textarea class="progress-iframe" placeholder="EXAMPLE: FAT LOSS, SPORT PREPARATION, FLEXABILITY"></textarea>
+				</div>
+				<div style="text-align:center;padding:10px 0;">
+					<button class="red-btn">Update</button>					
 				</div>
 			</div>
 		</div>
